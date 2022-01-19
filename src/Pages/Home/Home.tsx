a885089c2c5af8cd8f8c2e17720474ee68941767
Renzo Visperas <renzovisperas07@gmail.com>
2022-01-19 23:00:10 +0800

    delete left section, reuse featuredimage

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,10 @@
-import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
 import FeaturedImage from "./FeaturedImage";
 
 const Home = () => {
   return (
-    <main className="xs:flex xs:flex-col xs:relative lg:justify-center lg:items-center lg:flex-row lg:gap-6 xl:gap-20 lg:mt-20 xl:pb-10">
+    <main className="flex flex-col relative lg:justify-center lg:items-center lg:flex-row lg:gap-6  lg:mt-20 xl:gap-20 xl:pb-10">
       <FeaturedImage />
-      <LeftSection />
       <RightSection />
     </main>
   );
--- src/Pages/Home/Thumbnails.tsx
+++ src/Pages/Home/Thumbnails.tsx
@@ -15,8 +15,8 @@ const Thumbnails = () => {
     <>
       {thumbnails.map((thumbnail, index) => {
         return (
-          <button
-            className="relative border-2 border-opacity-0 rounded-xl overflow-hidden border-orange hover:border-opacity-100 w-full h-auto bg-orange bg-opacity-0 hover:bg-opacity-100"
+          <div
+            className="relative border-2 border-opacity-0 rounded-xl overflow-hidden border-orange hover:border-opacity-100 w-full h-auto bg-orange bg-opacity-0 hover:bg-opacity-100 cursor:pointer hidden lg:block"
             key={index}
             onClick={() => showLightBox(index)}
           >
@@ -26,7 +26,7 @@ const Thumbnails = () => {
               alt={`thumbnail${index}`}
             />
             <div className="absolute w-full h-full bg-white  top-0 left-0  bg-opacity-0 hover:bg-opacity-70"></div>
-          </button>
+          </div>
         );
       })}
     </>
