c5025ef5c7c61bc59fd8968729f398ec8119abff
Renzo Visperas <renzovisperas07@gmail.com>
2022-01-20 11:39:41 +0800

    fix bug with lightbox showing on mobile view

--- src/Pages/Home/FeaturedImage.tsx
+++ src/Pages/Home/FeaturedImage.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useAppSelector, useAppDispatch } from "../../store/hooks";
 import { appActions } from "../../store/appSlice";
 
@@ -5,6 +6,7 @@ import PreviousButton from "../../Components/PreviousButton/PreviousButton";
 import Thumbnails from "../Home/Thumbnails";
 
 const FeaturedImage = () => {
+  const [sizeIsOver1024px, setSizeIsOver1024px] = useState(false);
   const dispatch = useAppDispatch();
   const fullImages: string[] = useAppSelector(
     (state) => state.app.products[0].fullimages
@@ -22,9 +24,28 @@ const FeaturedImage = () => {
     dispatch(appActions.incrementActiveThumbnailIndex());
   };
 
-  const showLightBox = (index: number): void => {
-    dispatch(appActions.showLightBox(index));
+  const featuredImageClickHandler = (index: number): void => {
+    sizeIsOver1024px && dispatch(appActions.showLightBox());
+    changeLightBoxIndex(index);
   };
+
+  const changeLightBoxIndex = (index: number): void => {
+    dispatch(appActions.changeLightBoxIndex(index));
+  };
+
+  const html = document.querySelector("html")!;
+  const observer = new ResizeObserver((entries) => {
+    const html = entries[0];
+    setSizeIsOver1024px(html.contentRect.width >= 1024 ? true : false);
+  });
+  observer.observe(html);
+
+  useEffect(() => {
+    const closeLightBox = (): void => {
+      dispatch(appActions.closeLightBox());
+    };
+    !sizeIsOver1024px && closeLightBox();
+  }, [sizeIsOver1024px, dispatch]);
 
   return (
     <section className="relative lg:grid lg:w-1/3 lg:grid-cols-4 lg:gap-6 lg:auto-rows-max">
@@ -33,8 +54,10 @@ const FeaturedImage = () => {
         clickHandler={decrementActiveThumbnailIndex}
       />
       <img
-        onClick={() => showLightBox(activeThumbnailIndex)}
-        className="w-full lg:col-start-1 lg:col-span-4 lg:w-full lg:h-auto lg:rounded-xl"
+        onClick={() => featuredImageClickHandler(activeThumbnailIndex)}
+        className={`${
+          sizeIsOver1024px && "cursor-pointer"
+        } w-full lg:col-start-1 lg:col-span-4 lg:w-full lg:h-auto lg:rounded-xl`}
         src={fullImages[activeThumbnailIndex]}
         alt="shoe"
       />
